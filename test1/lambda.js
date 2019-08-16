3bb879e9066bd771aefacbf491698f55bd83c691
Indunil1992 <indunil@adroitlogic.com>
2019-08-16 10:11:31 +0530

    x

--- test1/lambda.js
+++ test1/lambda.js
@@ -1,62 +1,36 @@
 let AWS = require('aws-sdk');
-const sns = new AWS.SNS();
-const ses = new AWS.SES();
 const s3 = new AWS.S3();
 
 exports.handler = function (event, context, callback) {
-    s3.putObject({
-        "Body": "1",
-        "Bucket": "indunil1",
-        "Key": "10"
-    })
-        .promise()
+    s3.listObjects({
+        'Bucket': 'indunil.trigger',
+        'MaxKeys': 10,
+        'Prefix': '12'
+    }).promise()
         .then(data => {
             console.log(data);           // successful response
             /*
             data = {
-                ETag: "\"6805f2cfc46c0f04559748bb039d69ae\"",
-                VersionId: "pSKidl4pHBiNwukdbcPXAIs.sshFFOc0"
+                Contents: [
+                    {
+                       ETag: "\"70ee1738b6b21e2c8a43f3a5ab0eee71\"",
+                       Key: "example1.jpg",
+                       LastModified: "<Date Representation>",
+                       Owner: {
+                          DisplayName: "myname",
+                          ID: "12345example25102679df27bb0ae12b3f85be6f290b936c4393484be31bebcc"
+                       },
+                       Size: 11,
+                       StorageClass: "STANDARD"
+                    },
+                    // {...}
+                ]
             }
             */
         })
         .catch(err => {
             console.log(err, err.stack); // an error occurred
         });
-    ses.sendEmail({
-        Destination: {
-            ToAddresses: ['indunil@adroitlogic.com'],
-            CcAddresses: [],
-            BccAddresses: []
-        },
-        Message: {
-            Body: {
-                Text: {
-                    Data: 'As you can see, I gradually moved forward with Sigma IDE, with minimum prior knowledge of how to build a serverless application. Sigma handles all the hard works and delivers the coolest Serverless IDE to your hands.'
-                }
-            },
-            Subject: {
-                Data: 'subject test 1'
-            }
-        },
-        Source: 'hirudinee+aws@adroitlogic.com',
-    }, function (err, data) {
-        if (err) console.log(err, err.stack); // an error occurred
-        else console.log(data);           // successful response
-    });
-    sns.publish({
-        Message: 'test sample',
-        Subject: 'subject 1',
-        MessageAttributes: {},
-        MessageStructure: 'String',
-        TopicArn: 'arn:aws:sns:us-east-1:318300609668:amplify_codecommit_topic'
-    }).promise()
-        .then(data => {
-            // your code goes here
-        })
-        .catch(err => {
-            // error handling goes here
-        });
-
 
     callback(null, { "message": "Successfully executed" });
 }
